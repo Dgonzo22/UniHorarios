ee80339b667dd5843df8aedf0c027b57e23b37a1
unknown <juankls2@hotmail.com>
2025-09-01 22:20:39 -0500

    se creo horarios

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import ViewHome from "../views/ViewHome.vue";
 import Docentes from "../views/Docentes.vue";
+import Horarios from "../views/Horarios.vue"
 const routes = [
   { path: "/", name: "Home", component: ViewHome },
   { path: "/docentes", name: "Docentes", component: Docentes },
+  { path: "/Horarios", name: "Horarios", component: Horarios },
 ];
 
 const router = createRouter({
